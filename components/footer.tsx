b0fe1295f5851b02e999da46cea188883b5a84c3
chanmako <mgp7ry@gmail.com>
2021-01-26 17:59:20 +0900

    create footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/component/footer.module.scss';
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <small>Copyright &copy; 2020 chanmako All Rights Reserved.</small>
     </footer>
   );

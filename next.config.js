8e2704e98ee3968f99da2f7020f5e83d1018349b
chanmako <mgp7ry@gmail.com>
2021-01-26 13:40:32 +0900

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable
+  @typescript-eslint/no-var-requires
+*/
+
+const { resolve } = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    // src ディレクトリをエイリアスのルートに設定
+    config.resolve.alias['~'] = resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
